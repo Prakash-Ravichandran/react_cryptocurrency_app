34d36820f06aab97eb0368cf33743a69a4e96f9e
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2023-09-21 16:39:46 +0530

    Feat: HomePage Component Markups

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Typography, Statistic, Row, Col } from "antd";
+import { Link } from "react-router-dom";
+
+const { Title } = Typography;
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Title level={2} className="heading">
+        Global Crypto Statistics
+      </Title>
+      <Row>
+        <Col>
+          <Statistic span={12} title="Total Cryptocurrencies"></Statistic>
+          <Statistic span={12} title="Total Exchanges"></Statistic>
+          <Statistic span={12} title="Total Market Cap"></Statistic>
+          <Statistic span={12} title="Total 24h Volume"></Statistic>
+          <Statistic span={12} title="Total Markets"></Statistic>
+        </Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+      </Row>
+    </div>
+  );
 };
 
 export default HomePage;
