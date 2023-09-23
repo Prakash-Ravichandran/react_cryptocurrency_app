14afd358900c14fc8a7755709765d0b73857c4c6
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2023-09-23 16:13:04 +0530

    Fix: removed antd.css

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useGetCrytoMarketsQuery } from "../services/cryptoMarketsApi";
 
 const Crytocurrencies = () => {
+  const { data, isFetching } = useGetCrytoMarketsQuery();
+  console.log(data);
   return <div>Crytocurrencies</div>;
 };
 
