a289ba038dc910d5ee34a675100661614cff4b70
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2023-09-27 21:37:06 +0530

    Feat: News Component Markups, Line Clamps

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,12 +1,11 @@
 import { Card, Row, Col } from "antd";
-import { Typography } from "antd";
+import { Typography, Avatar } from "antd";
+import moment from "moment/moment";
 import React, { useEffect } from "react";
 import { useState } from "react";
-
-import { useGetCrytoMarketsQuery } from "../services/cryptoMarketsApi";
 import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 
-const { Title } = Typography;
+const { Title, Text } = Typography;
 const demoImage =
   "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
 
@@ -23,7 +22,7 @@ const News = ({ simplified }) => {
 
   return (
     <>
-      <Row gutter={[24, 24]}>
+      <Row gutter={[24, 24]} className="news-row">
         {value.map((newsItem, index) => {
           return (
             <Col xs={24} sm={12} lg={8} key={index}>
@@ -31,18 +30,39 @@ const News = ({ simplified }) => {
                 <a href={newsItem.url} target="_blank" rel="noreferrer">
                   <div className="news-image-container">
                     <Title className="news-title" level={4}>
+                      {/* {newsItem.name.length > 50
+                        ? `${newsItem.name.substring(0, 100)}...`
+                        : newsItem.name} */}
                       {newsItem.name}
                     </Title>
-                    <img
-                      src={newsItem?.image?.thumbnail?.contentUrl || demoImage}
-                      alt="news"
-                    />
+                    <span>
+                      <img
+                        src={
+                          newsItem?.image?.thumbnail?.contentUrl || demoImage
+                        }
+                        alt="news"
+                      />
+                    </span>
                   </div>
                   <p>
                     {newsItem.description.length > 100
                       ? `${newsItem.description.substring(0, 100)}...`
                       : newsItem.description}
                   </p>
+                  <div className="provider-container">
+                    <div>
+                      <Avatar
+                        src={
+                          newsItem.provider[0]?.image?.thumbnail?.contentUrl ||
+                          demoImage
+                        }
+                      ></Avatar>
+                      <Text>{newsItem.provider[0].name}</Text>
+                    </div>
+                    <Text>
+                      {moment(newsItem.datePublished).startOf("ss").fromNow()}
+                    </Text>
+                  </div>
                 </a>
               </Card>
             </Col>
