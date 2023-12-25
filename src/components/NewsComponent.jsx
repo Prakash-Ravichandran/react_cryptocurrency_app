932b9faf1c25163312c8d9d6aecd150bc9421a8e
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2023-12-25 23:22:17 +0530

    [fix]: news component

--- src/components/NewsComponent.jsx
+++ src/components/NewsComponent.jsx
@@ -8,7 +8,7 @@ const demoImage =
 const NewsComponent = () => {
   const { data: news, isFetching } = useGetgeneralNewsApiQuery();
 
-  console.log(JSON.stringify(news?.articles));
+  //   console.log(JSON.stringify(news?.articles));
 
   if (isFetching) return "Loading ...";
   return (
