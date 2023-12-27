6859737e8524fd652f6f24c9cc9c438f11bfcc67
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2023-12-27 08:25:55 +0530

    [feat]: dropdwon component - api news category

--- src/components/NewsComponent.jsx
+++ src/components/NewsComponent.jsx
@@ -1,11 +1,25 @@
-import { Avatar, Card, Col, Row, Typography } from "antd";
-import React from "react";
+import { Avatar, Card, Col, Row, Select, Typography } from "antd";
+import React, { useState } from "react";
 import { useGetgeneralNewsApiQuery } from "../services/generalNewsApi";
 const { Title, Text } = Typography;
+const { Option } = Select;
 const demoImage =
   "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
 
+const category = [
+  "general",
+  "world",
+  "nation",
+  "business",
+  "technology",
+  "entertainment",
+  "sports",
+  "science",
+  "health",
+];
+
 const NewsComponent = () => {
+  const [newsCategory, setNewsCategory] = useState("general");
   const { data: news, isFetching } = useGetgeneralNewsApiQuery();
 
   console.log("News=" + JSON.stringify(news?.articles));
@@ -14,11 +28,30 @@ const NewsComponent = () => {
   return (
     <>
       <Row gutter={[24, 24]} className="news-row">
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a news category"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase())
+            }
+          >
+            {category.map((value, id) => (
+              <Option value={value} key={id}>
+                {value}
+              </Option>
+            ))}
+          </Select>
+        </Col>
+
         {news?.articles?.map((value, id) => {
           return (
             <>
               <Col xs={24} sm={12} lg={8} key={id}>
-                <Card>
+                <Card key={id}>
                   <a href={value?.url} target="_blank" rel="noreferrer">
                     <div className="news-image-container">
                       <Title className="news-title" level={4}>
