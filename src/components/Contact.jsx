d3ccc6f05176277eb1e0359f5ae49cb19ac1fd97
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2024-01-07 17:25:16 +0530

    [feat]: integrate form icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,19 @@
-import { FormLabel, TextField } from "@mui/material";
+import {
+  AccountCircle,
+  Description,
+  Email,
+  KeyboardArrowRight,
+} from "@mui/icons-material";
+import {
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  InputAdornment,
+  Radio,
+  RadioGroup,
+  TextField,
+} from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import React from "react";
 
@@ -31,6 +46,13 @@ const Contact = () => {
           fullWidth
           required
           className={classes.field}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          }}
         />
         <TextField
           label={"Email"}
@@ -39,6 +61,13 @@ const Contact = () => {
           fullWidth
           required
           className={classes.field}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Email></Email>
+              </InputAdornment>
+            ),
+          }}
         />
         <TextField
           placeholder="Describe the best about your favourite bitcoin !"
@@ -50,7 +79,32 @@ const Contact = () => {
           className={classes.field}
           multiline
           rows={3}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Description />
+              </InputAdornment>
+            ),
+          }}
         />
+        <FormControl className={classes.field}>
+          <FormLabel>Your rating for the cryptocurrencies</FormLabel>
+          <RadioGroup name="rating" value={"5"}>
+            <FormControlLabel value={"5"} control={<Radio />} label="5" />
+            <FormControlLabel value={"4"} control={<Radio />} label="4" />
+            <FormControlLabel value={"3"} control={<Radio />} label="3" />
+            <FormControlLabel value={"2"} control={<Radio />} label="2" />
+            <FormControlLabel value={"1"} control={<Radio />} label="1" />
+          </RadioGroup>
+        </FormControl>
+        <Button
+          type="submit"
+          variant="outlined"
+          color="primary"
+          endIcon={<KeyboardArrowRight />}
+        >
+          Submit
+        </Button>
       </form>
     </>
   );
